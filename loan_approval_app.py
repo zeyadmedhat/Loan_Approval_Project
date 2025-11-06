6a407166e0ac1b41900cfd7127a113dadcd8870f
Zeyad Medhat <148156103+zeyadmedhat@users.noreply.github.com>
2025-11-06 08:37:27 +0200

    Add files via upload

--- loan_approval_app.py
+++ loan_approval_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
 import joblib
 import plotly.graph_objects as go
 import plotly.express as px
